9dc6112db3acb277ae335cf65d094f080d05112e
amkhrjee <aniruddha.mukherji@outlook.com>
2024-06-15 19:19:55 +0530

    add: projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { Avatar, Card, CardBody, Link, Tab, Tabs } from "@nextui-org/react";
+import {
+  Avatar,
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Link,
+  Tab,
+  Tabs,
+} from "@nextui-org/react";
 
 function App() {
   return (
@@ -48,7 +57,8 @@ function App() {
         and thus, can do my fair share of web development. These days I tend to
         spend my free time learning ML and AI.
         <br />
-        <br />I am open to internships and work. You can view and download my CV{" "}
+        <br />I am open to internships and work opportunities. You can view and
+        download my CV{" "}
         <Link
           isExternal
           showAnchorIcon
@@ -60,32 +70,120 @@ function App() {
       </div>
       <br />
       <div className="flex justify-center w-full flex-col">
-        <Tabs aria-label="Options">
-          <Tab key="photos" title="Photos">
+        <Tabs className="justify-center" aria-label="Options">
+          <Tab key="photos" title="Recent Projects">
             <Card>
+              <CardHeader className="flex-col items-start">
+                <div>BMI Estimation from Images</div>
+                <div>
+                  <Link
+                    href="https://bmiweb.amkhrjee.xyz"
+                    isExternal
+                    showAnchorIcon
+                  >
+                    bmiweb.amkhrjee.xyz
+                  </Link>
+                </div>
+              </CardHeader>
+              <Divider />
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                Tries to estimate BMI from image. MTCNN for face extraction.
+                Simple LR for BMI model (as of now). Made as an university
+                project.
+              </CardBody>
+            </Card>
+            <br />
+            <Card>
+              <CardHeader className="flex-col items-start">
+                <div>Fontonic Browser Extension</div>
+                <div>
+                  <Link
+                    href="https://fontonic.amkhrjee.xyz"
+                    isExternal
+                    showAnchorIcon
+                  >
+                    fontonic.amkhrjee.xyz
+                  </Link>
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                300+ users. Let's you customize the fonts of any website with
+                locally installed fonts. Cross-browser compatible. Written
+                without any frameworks.
+              </CardBody>
+            </Card>
+            <br />
+            <Card>
+              <CardHeader className="flex-col items-start">
+                <div>Automation of 3D printing process</div>
+                <div>
+                  <Link
+                    href="https://proshandstore.amkhrjee.xyz"
+                    isExternal
+                    showAnchorIcon
+                  >
+                    proshandstore.amkhrjee.xyz
+                  </Link>
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                Automated the entire pipeline of ordering and 3D printing of
+                prosthetic hands. Made Windows native client for admins to
+                manage orders. Made as university project.
+              </CardBody>
+            </Card>
+            <br />
+            <Card>
+              <CardHeader className="flex-col items-start">
+                <div>MIT OpenCourseWare Downloader (CLI)</div>
+                <div>
+                  <Link
+                    href="https://github.com/amkhrjee/ocwd"
+                    isExternal
+                    showAnchorIcon
+                  >
+                    github.com/amkhrjee/ocwd
+                  </Link>
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                350+ users (pwsh-only metrics; not counting bash installs).
+                Let's you download course materials for MIT courses in bulk at
+                ease. Pure shell scripts.
+              </CardBody>
+            </Card>
+            <br />
+            <Card>
+              <CardHeader className="flex-col items-start">
+                <div>PyGraphia (Python library)</div>
+                <div>
+                  <Link
+                    href="https://github.com/amkhrjee/ocwd"
+                    isExternal
+                    showAnchorIcon
+                  >
+                    github.com/amkhrjee/ocwd
+                  </Link>
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                350+ users (pwsh-only metrics; not counting bash installs).
+                Let's you download course materials for MIT courses in bulk at
+                ease. Pure shell scripts.
               </CardBody>
             </Card>
           </Tab>
-          <Tab key="music" title="Music">
+          <Tab key="music" title="Latest Blog Posts">
             <Card>
               <CardBody>
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco
                 laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
                 dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur.
-              </CardBody>
-            </Card>
-          </Tab>
-          <Tab key="videos" title="Videos">
-            <Card>
-              <CardBody>
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim id est laborum.
               </CardBody>
             </Card>
           </Tab>
