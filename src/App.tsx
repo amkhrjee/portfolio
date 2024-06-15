df80d7c257c8664c66701a4ceed9d9c859d1c205
amkhrjee <aniruddha.mukherji@outlook.com>
2024-06-15 17:15:02 +0530

    fix: adjusted for small phones

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { Avatar, Link } from "@nextui-org/react";
+import { Avatar, Card, CardBody, Link, Tab, Tabs } from "@nextui-org/react";
 
 function App() {
   return (
     <>
       <div className="flex gap-4 items-center w-full">
         <div className="">
-          <Avatar
-            style={{
-              width: "6rem",
-              height: "6rem",
-            }}
-            src="./assets/potrait.jpg"
-            size="lg"
-          />
+          <Avatar src="./assets/potrait.jpg" size="lg" />
         </div>
         <div>
           <p
             style={{
-              fontSize: "1.5rem",
+              fontSize: "1.2rem",
               fontWeight: "600",
             }}
           >
@@ -64,6 +57,39 @@ function App() {
           here
         </Link>
         .
+      </div>
+      <br />
+      <div className="flex justify-center w-full flex-col">
+        <Tabs aria-label="Options">
+          <Tab key="photos" title="Photos">
+            <Card>
+              <CardBody>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="music" title="Music">
+            <Card>
+              <CardBody>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="videos" title="Videos">
+            <Card>
+              <CardBody>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.
+              </CardBody>
+            </Card>
+          </Tab>
+        </Tabs>
       </div>
     </>
   );
