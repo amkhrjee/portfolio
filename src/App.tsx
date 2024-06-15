0c74355201bdd29d659b813910daa40a18c29373
amkhrjee <aniruddha.mukherji@outlook.com>
2024-06-15 15:44:31 +0530

    nextui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import viteLogo from "/Boy.svg";
-import "./App.css";
-
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <button
-        onClick={() => {
-          location.href = "https://bento.me/amkhrjee";
-        }}
-      >
-        @amkhrjee
-      </button>
-      <h2>Hi, this site is under heavy construction.</h2>
+      <h1 className="text-center">Hello World</h1>
     </>
   );
 }
