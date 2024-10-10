acfe2962a0177a4e0c9151c3ecb751551f1bff86
amkhrjee <aniruddha.mukherji@outlook.com>
2024-10-10 07:35:20 +0530

    rm: react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
-import { nextui } from "@nextui-org/react";
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js, ts, jsx, tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./*.html"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Taviraj", "sans-serif", "system-ui"],
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  daisyui: {
+    themes: ["dracula"],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
