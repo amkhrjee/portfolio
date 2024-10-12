dbd8aacd38f728a113b2a0059d0266c93a9eaafd
amkhrjee <aniruddha.mukherji@outlook.com>
2024-10-12 08:10:28 +0530

    rm: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-    },
-  },
   daisyui: {
     themes: ["emerald", "dark"],
   },
