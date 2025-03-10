94c90e19de2959486c2c758105d1e8e8cafca243
amkhrjee <aniruddha.mukherji@outlook.com>
2025-03-10 18:06:55 +0530

    fix: chart dependency

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -2,8 +2,12 @@
 
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import { cn } from "@/lib/utils";
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const;
