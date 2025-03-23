ab6e219aad7ab8479f0400c9b9b4a9cf42dbc7ad
amkhrjee <aniruddha.mukherji@outlook.com>
2025-03-23 06:15:57 -0700

    rm: monospace font

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,8 +1,3 @@
-import { Inter_Tight, JetBrains_Mono } from "next/font/google";
+import { Inter_Tight } from "next/font/google";
 
 export const fontSans = Inter_Tight({ subsets: ["latin"] });
-
-export const fontMono = JetBrains_Mono({
-  weight: "variable",
-  subsets: ["latin"],
-});
