ad64a7f7546950adc998229eee108b6d22895689
amkhrjee <aniruddha.mukherji@outlook.com>
2025-03-27 08:28:56 -0700

    add: revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import HomeProvider from "@/components/ui/home-provider";
 import Spotify from "@/components/ui/spotify";
 
+export const revalidate = 60;
+
 export default function Home() {
   return (
     <HomeProvider>
